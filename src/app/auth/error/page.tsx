6f19bf7c3f538634f71f069342a2fb1511c724bf
Prajwal-kp-18 <prajwal.kp.1817@gamil.com>
2024-11-15 18:55:56 +0530

    completed the build after adding the govId and details to login and register

--- src/app/auth/error/page.tsx
+++ src/app/auth/error/page.tsx
@@ -1,7 +1,12 @@
 import { ErrorCard } from "@/components/auth/error-card";
+import React, { Suspense } from "react";
 
 const AuthErrorPage = () => {
-  return <ErrorCard />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ErrorCard />
+    </Suspense>
+  );
 };
 
 export default AuthErrorPage;
--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,7 +1,11 @@
 import { RegisterForm } from "@/components/auth/register-form";
-
+import React, { Suspense } from "react";
 const RegisterPage = () => {
-  return <RegisterForm />;
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <RegisterForm />
+    </Suspense>
+  );
 };
 
 export default RegisterPage;
