a8ba9ed4f3e7d8b17a13b401fe015b9815a340c7
copypasting-warrior <baruaharyan251@gmail.com>
2024-11-17 11:28:19 +0530

    added not found

---
+++ src/app/not-found.tsx
@@ -0,0 +1,3 @@
+export default function NotFound(){
+    return <div class="w-full h-screen text-center text-6xl bg-black text-white pt-96" className="notfound" ><h2>404 | This page could not be found</h2></div>;
+}
