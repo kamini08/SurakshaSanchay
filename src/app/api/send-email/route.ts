e32147d0e8d0ce934de6e5edfa81b26c13c64414
BhoomiAgrawal12 <167442415+BhoomiAgrawal12@users.noreply.github.com>
2024-12-07 22:00:26 +0530

    Update route.ts(of sending automatic mail)

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import checkAndSendNotifications from '@/lib/utils/notificationLogic';
 
-export async function GET(req: NextRequest) {
+export async function POST(req: NextRequest) {
   try {
     await checkAndSendNotifications();
     return NextResponse.json({ message: 'Notifications sent successfully' });
