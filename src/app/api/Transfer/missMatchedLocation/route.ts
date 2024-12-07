050398ccdfecf1afb9b091409b351083fe988aa5
BhoomiAgrawal12 <167442415+BhoomiAgrawal12@users.noreply.github.com>
2024-12-08 05:13:47 +0530

    fir kuch hua hai

---
+++ src/app/api/Transfer/missMatchedLocation/route.ts
@@ -0,0 +1,54 @@
+
+//  NOT REQUIRED NOW!
+
+// import { db } from "@/lib/db";
+// import { auth } from "../../../../../auth";
+
+// export async function PUT(req: Request) {
+//     try {
+//         const session = await auth();
+//         const user = session?.user;
+
+//         const locationUser = await db.user.findUnique({
+//             where: { id: user?.id },
+//             select: { location: true, govId: true }
+//         })
+//         const issuedTo = await db.inventoryItem.findUnique({
+//             where: { AND: [{ location: locationUser?.location }, { NOT: [{issuedTo: locationUser?.govId,}] },] },
+//             select: { issuedTo: true }
+//         })
+//         const falseLocation = await db.user.findUnique({
+//             where: { govId: issuedTo?.issuedTo },
+//             select: { location: true }
+//         }
+//     });
+
+//     const mismatchedItems = await db.inventoryItem.findMany({
+//         where: {
+//             AND: [
+//                 {
+//                     location: {
+//                         not: null, // Ensures current location exists
+//                     },
+//                 },
+//                 {
+//                     issuedTo: {
+//                         not: null, // Ensures issuedTo exists
+//                     },
+//                 },
+//                 {
+//                     NOT: {
+//                         falseLocation: { equals: locationUser.location },
+//                     }
+//                 },
+//             ],
+//         },
+//     });
+//     return new Response(JSON.stringify(mismatchedItems), { status: 200 });
+// } catch (error) {
+//     console.error("Error fetching mismatched locations:", error);
+//     return new Response(JSON.stringify({ error: "Failed to fetch data" }), {
+//         status: 500,
+//     });
+// }
+// }
